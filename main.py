1bdedad1c80996e86b8b21d8b270a7305e134ed2
Dan Burk <danburk10+github@gmail.com>
2018-01-09 02:17:51 +0000

    Update main.py
    more progress, parse the second search result, add picture to tk. using a local file to avoid constantly hitting amazon.

--- main.py
+++ main.py
@@ -1,52 +1,79 @@
-from Tkinter import *
+#from Tkinter import *
+import Tkinter as tk
 import urllib2
+import urllib
+import cStringIO
 import webbrowser
 import os
+from BeautifulSoup import BeautifulSoup
+from PIL import ImageTk, Image
+
+class Product(object):
+	def __init__(self, title, href, asin, price):
+		self.title = title
+		self.href = href
+		self.asin = asin
+		self.price = price
+	
+	
+def callback(event):
+	print "call callback"
+    #webbrowser.open_new(event.widget.cget("text"))
+    
+
+url = 'file://' + urllib.quote(os.path.abspath("./amzsrch.html"))
+page = urllib2.urlopen(url)
+#print page.read()
+soup = BeautifulSoup(page.read())
+#print soup.prettify()
+
+
+
+resultListFindParameters = "ul", {"id":"s-results-list-atf"}
+
+#result_list = soup.find("ul", {"id":"s-results-list-atf"})
+result_list = soup.find(resultListFindParameters)
+#print result_list.prettify()
 
 
 
 
-#class webScrape(object):
-#    def __init__(self, queryString):
-#        self.url = baseUrl+queryString
-#        reponse urllib2.urlopen(self.url)
-        
-        
+result1 = result_list.find("li", {"id":"result_1"})
+result1Title = result1.find("h2")["data-attribute"]
+result1Href = result1.find("a")["href"]
+result1Asin = result1["data-asin"]
+result1PriceSpan = result1.find("span", {"class":"sx-price sx-price-large"})
+result1ImgSrc = result1.find("img")["src"]
 
 
-class Application(Frame):
-    def search(self, event):
-        #results = webScrape()
-        
-        url = 'https://www.google.com/#q='
-        url=url+self.searchEntry.get()
-        print url
-        
-        #for right now do now open url, the below code works fine
-        #response = urllib2.urlopen(url+self.searchEntry.get())
-        #html = response.read()
-        #print html
-        
+file = cStringIO.StringIO(urllib.urlopen(result1ImgSrc).read())
+img = Image.open(file)
 
-    def createWidgets(self):
-        self.searchEntry = Entry(self, text="type something here", width=30)
-        self.searchEntry.bind('<Return>', self.search)
-        self.searchEntry.pack({"side": "left"})
+root = tk.Tk()
 
-        self.searchBtn = Button(self, text="search")
-        self.searchBtn.bind('<Button-1>', self.search)
-        self.searchBtn.pack({"side": "left"})
-
-#    def displaySearchResult():
-        
+tkimg = ImageTk.PhotoImage(img)
+imglabel = tk.Label(root, image=tkimg, cursor="hand") #.grid(row=1, column=1)
+#Label(root, text=result1Asin, borderwidth=2, relief="solid").grid(row=1,column=2)
+#lbl = Label(root, text="hello", fg="blue", cursor="hand2")
+imglabel.pack()
+imglabel.bind("<Button-1>", callback)
+#Label(root, text=result1Href).grid(row=1,column=3)
+#Label(root, text=result1Title).grid(row=1,column=3)
+#Label(root, text=result1PriceSpan.text[:-2] + '.' + result1PriceSpan.text[-2:]).grid(row=1,column=4)
+#print result1PriceSpan
 
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+#root.pack()
+root.mainloop()
 
-root = Tk()
-app = Application(master=root)
-app.mainloop()
+#print result1Title
+#print result1Href
+#print result1Asin
+#print result1PriceSpan.text[:-2] + '.' + result1PriceSpan.text[-2:]
+#print "*"
+
+
+#root = Tk()
+#app = Application(master=root)
+#app.mainloop()
 #root.destroy()
